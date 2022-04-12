3d08a71bf4f70dc7811d6b4abb4db2c8b62135e9
Greatest125 <66028078+Greatest125@users.noreply.github.com>
2022-04-11 20:48:10 -0400

    screw deprecation

--- DRS/facility_origin.py
+++ DRS/facility_origin.py
@@ -1,45 +1,4 @@
-#Downloads RDS (data <=2019) Facility data (with origin data) which does not track waste type from this website: https://www2.calrecycle.ca.gov/LGCentral/DisposalReporting/Origin/FacilitySummary
-
-import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-import chromedriver_autoinstaller
-import time
-import glob
-import os
-
-chromedriver_autoinstaller.install()  
-chromeOptions = webdriver.ChromeOptions()
-prefs = {"download.default_directory" : "/home/leeld/Downloads/files"}
-#change the directory to the whatever you want
-chromeOptions.add_experimental_option("prefs",prefs)
-#chromedriver = "/usr/bin/chromedriver"
-#you need to change the path of the chromedriver depending on your setup (I'm on Lubuntu 20.04)
-driver = webdriver.Chrome(options=chromeOptions)
-wait = WebDriverWait(driver, 20)
-
-url = "https://www2.calrecycle.ca.gov/LGCentral/DisposalReporting/Origin/FacilitySummary"
-
-driver.get(url)
-dropdown = driver.find_element(By.ID, "ReportType")
-dropdown.find_element(By.XPATH, "//option[. = 'Jurisdiction of Origin Waste Disposal']").click()
-select_el = wait.until(EC.presence_of_element_located((By.XPATH, '//select[@id="SWISNumber"]')))
-facilities = select_el.find_elements(By.TAG_NAME, 'option')
-total_facilities = len(facilities)
-
-# len - 1 because the 1st option is blank
-print(f"Total facilities: {total_facilities - 1}")
-btn = driver.find_element(By.XPATH, '//button[@id="SearchButton"]')
-
-for i in range(1, len(facilities)):
-    print(f"Clicking Facility - {facilities[i].text}")
-    facilities[i].click()
-    btn.click()
-    time.sleep(2)
-driver.close()
-
+import os, pandas as pd, glob
 #Merge all of the spreadsheets (one for each facility) into one master spreadsheet 
 os.chdir("/home/leeld/Downloads/files")
 extensions = ("*xlsx")
@@ -62,7 +21,7 @@ for file in filenames:
    #Clean up unwanted text from spreadsheet
    df = df[~df['Unnamed: 0'].isin(['Jurisdiction or Origin Waste Disposal By Facility'])]
    # appending data of excel files
-   outputxlsx = outputxlsx.concat( df, ignore_index=True)
+   outputxlsx = outputxlsx.append( df, ignore_index=True)
 print('All spreadsheets merged into one file ("RDS_FacilityReports(origin data).xlsx"')
 outputxlsx.to_excel("/home/leeld/Downloads/files/RDS_FacilityReports(origin data).xlsx", index=False)
 #Delete data files for each county
